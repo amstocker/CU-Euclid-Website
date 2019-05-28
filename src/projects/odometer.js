49ec19f8b90e20c41b81f85bccee530eece5f6f2
Andrew Stocker <sleepyams@gmail.com>
2019-05-28 06:39:34 +0000

    bit twiddlin speedup

--- src/projects/odometer.js
+++ src/projects/odometer.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { setupCanvas, interpolateHex } from '../utils.js';
 
-const SUBDIVS = 7;
+const N = 9;  // number of subdivisions
 
 const SEG_MID = 1;  // pixels
 
@@ -14,10 +14,10 @@ const SELECTED_COLOR = "#f442c5";
 
 const GRID_SIZE = 900;
 const GRID_WIDTH = 50;
-const BASIS_WIDTH = 20;
+const BASIS_WIDTH = 8;
 const BASIS_SPACING = 2;
 
-const ACTION_ITERS = 20;
+const ACTION_ITERS = 10;
 const ACTION_COLORS = Array
     .from(Array(ACTION_ITERS).keys())
     .map((i) => interpolateHex(SELECTED_COLOR, GRID_COLOR, i*(1/ACTION_ITERS)));
@@ -37,7 +37,7 @@ class Odometer {
         // make segments
         this.segments = [[0, this.size]];
         this.bases = [this.segments];
-        for (let i = 0; i < SUBDIVS; i++) {
+        for (let i = 0; i < N; i++) {
             let new_segments = [];
             for (let seg of this.segments) {
                 let w = seg[1] - seg[0];
@@ -52,87 +52,61 @@ class Odometer {
     }
 
     seg_to_coord (seg) {
-        let c = [];
-        let l = 0, r = this.size;
-        for (let k = 0; k < SUBDIVS; k++) {
-            let w = r - l;
-            let x = (w+SEG_MID)/2;
-            let t = Number(seg[1] > l + x);
-            c.push(t);
-            l += t * x;
-            r -= (1-t) * x;
-        };
-        return c;
+        // coord is an integer between 0 and (2^N - 1)
+        return Math.floor((seg[0]/this.size) * (2**N))
+    }
+
+    pos_to_coord (x) {
+        return Math.floor((x/this.size) * (2**N));
     }
 
     coord_to_seg (coord) {
-        let i = 0;
-        for (let k = 0; k < SUBDIVS; k++) {
-            i += coord[k] * 2**(SUBDIVS-k-1);
-        };
-        return this.segments[i];
+        return this.segments[coord];
     }
 
     coord_to_text (coord) {
         let t = "(";
-        for (let i = 0; i < coord.length; i++) {
-            t += coord[i] + ", ";
+        for (let i = 0; i < N; i++) {
+            t += Number((coord & (1 << (N-i-1))) != 0)  + ", ";
         };
         t += "...)"
         return t;
     }
 
-    group_action (seg) {
-        let coord = this.seg_to_coord(seg);
-        if (coord[0]) {
-            let carry = true, k = 1;
-            coord[0] = 0;
-            while (carry) {
-                if (coord[k]) {
-                    k++;
-                    if (k == SUBDIVS) {
-                        return new Array(SUBDIVS).fill(0);
-                    };
-                } else {
-                    coord[k] = 1;
-                    carry = false;
-                };
-            };
-        } else {
-            coord[0] = 1;
+    group_action (x) {
+        let mask = 1 << (N-1);
+        while (mask) {
+            let carry = x & mask;
+            x = x ^ mask;
+            mask = carry >> 1;
         }
-        return this.coord_to_seg(coord);
+        return x;
     }
 
     update (ctx, abs_x, abs_y) {
         let rel_x = Mouse.x - abs_x;
-        if (rel_x < 0 || rel_x > this.size) {
+        if (rel_x < 0 || rel_x >= this.size) {
             this.selected = undefined;
             return;
         }
-        for (let seg of this.segments) {
-            if (rel_x >= seg[0] && rel_x <= seg[1]) {
-                this.selected = seg;
-                return;
-            }
-        };
+        this.selected = this.pos_to_coord(rel_x);
     }
 
     render (ctx, abs_x, abs_y) {
         // draw Cantor grid
         ctx.fillStyle = GRID_COLOR;
-        for (let seg of this.segments) {
+        for (let i = 0; i < this.segments.length; i++) {
+            let seg = this.segments[i];
+            ctx.fillStyle = (i == this.selected) ? SELECTED_COLOR : GRID_COLOR;
             ctx.fillRect(abs_x+seg[0], abs_y-this.thickness, seg[1]-seg[0], this.thickness);
         };
 
         // draw base sets
-        for (let j = 1; j < SUBDIVS + 1; j++) {
-            let base = this.bases[j];
-            for (let seg of base) {
-                if (this.selected
-                     && this.selected[0] >= seg[0]
-                     && this.selected[1] <= seg[1])
-                {
+        for (let j = 1; j < N; j++) {
+            let base = this.bases[N-j];
+            for (let i = 0; i < base.length; i++) {
+                let seg = base[i];
+                if (this.selected >> j == i) {
                     ctx.fillStyle = SELECTED_COLOR;
                 } else {
                     ctx.fillStyle = GRID_COLOR;
@@ -145,24 +119,11 @@ class Odometer {
             };
         };
 
-        // draw iterations of group action of selected element and nbhd filter
-        if (this.selected) {
-            let seg = this.selected;
-            for (let i = 0; i < ACTION_ITERS; i++) {
-                ctx.fillStyle = ACTION_COLORS[i];
-                ctx.fillRect(abs_x+seg[0], abs_y-this.thickness, seg[1]-seg[0], this.thickness);
-                seg = this.group_action(seg);
-            };
-
-
-        };
-
         // draw label
-        if (this.selected) {
+        if (this.selected > -1) {
             ctx.fillStyle = FONT_COLOR;
             ctx.font = FONT;
-            let c = this.seg_to_coord(this.selected);
-            ctx.fillText(this.coord_to_text(c), abs_x + 0, abs_y - this.thickness - 10);
+            ctx.fillText(this.coord_to_text(this.selected), abs_x + 0, abs_y - this.thickness - 10);
         };
     }
 };
@@ -208,7 +169,7 @@ class Main extends React.Component {
     animate() {
         const [cvs, ctx, w, h] = this.getCanvasInfo();
         let abs_x = (w - this.odometer.size)/2,
-            abs_y = h/2;
+            abs_y = 1/3*h;
         
         // clear background
         ctx.clearRect(0, 0, w, h);
